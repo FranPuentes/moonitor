c5375b99c333fc59f10e9e9b9e1c51ae57cb71fb
fran-puentes <fran@puentescalvo.com>
2012-12-02 18:09:28 +0100

    comandos iamalive, ping/pong, plugins, plugin

--- broker/main.js
+++ broker/main.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 /*************************************************************
+  MOONITOR broker
   (c) 2012, Francisco Puentes (fran@puentescalvo.com)
 *************************************************************/
 var Util =require('util');
@@ -11,19 +12,74 @@ var Dgram=require('dgram');
 var NODE=process.argv[0];
 var SELF=Path.resolve(process.argv[1]);
 var CWD =Path.dirname(SELF);
+var PID =Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.pid');
+var KILL=Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.kill');
+var CONF=Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.conf');
 
 var Tools =require(Path.join(CWD,'common/tools.js' ));
 var Level5=require(Path.join(CWD,'common/level5.js'));
 
 var log=console.log;
 
-//TODO: comprobar si existe un *.pid ejecutándose
-//      si es así, abandonar esta instancia
+if(Fs.existsSync(PID))
+  {
+   try
+     {
+      var oldPid=parseInt(Fs.readFileSync(PID),10);
+      try
+        {
+         process.kill(oldPid,'SIGUSR2');
+         console.log("There are other instance of me working hard :-)");
+         process.exit(0);
+        }
+      catch(err)
+        {
+         Fs.unlinkSync(PID);
+        }
+     }
+   catch(err)
+     {
+      console.err(err);
+      console.err("Failed to load and parse '"+PID+"'");
+      process.exit(1);
+     }
+  }
 
-//create a '*.pid' file into CWD, with our PID
-Fs.writeFileSync(Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.pid'),process.pid,'utf8');
-//'ps' will report this name
-process.title="moon-broker";
+Fs.writeFileSync(PID,process.pid,'utf8');
+
+process.on('exit',
+           function()
+           {
+            console.log("Ending ...");
+            if(Tools.isset(server))
+              {
+               server.close();
+               console.log("Link closed");
+              }
+            if(Tools.isset(http))
+              {
+               http.close();
+               console.log("Http closed");
+              } 
+            Fs.unlinkSync(PID);
+           });
+
+process.on('SIGINT',
+           function()
+           {
+            console.log("INT signal!");
+            process.exit(0);
+           });
+
+process.on('SIGUSR2',
+           function()
+           {
+            console.log("USR2 signal!");
+           });
+
+process.title="moonitor::broker";
+
+//TODO: crear el script KILL que mata elegantemente esta instancia
 
 var conf=
     {
@@ -35,13 +91,18 @@ var conf=
        [
         { name:'localnet', broadcast:'127.255.255.255', port:1233 },
        ],
+     http:
+       {
+        bind:{ address:'127.0.0.1', port:8080 },
+        pages:{ static:'web/static', nonstatic:'web/nonstatic' },
+       }
     };
 
-if(Fs.existsSync(Path.join(CWD,'broker.conf.json')))
+if(Fs.existsSync(CONF))
   {
    try
      {
-      var tmp=Fs.readFileSync(Path.join(CWD,'broker.conf.json'));
+      var tmp=Fs.readFileSync(CONF);
       var conf=JSON.parse(tmp);
      }
    catch(err)  
@@ -94,6 +155,23 @@ function insertOrUpdate(table,keys,data)
  return id;
 }
 
+function existsKey(table,keys)
+{
+ for(var i=0; i<table.length; i++)
+    {
+     var row=table[i];
+
+     var all=true;
+     for(var key in keys)
+        {
+         if(!Tools.isset(row[key]) || row[key]!==keys[key]) { all=false; break; }
+        }
+
+     if(all===true) return i;
+    }
+ return 0;
+}
+
 /////////////////////////////////////////////////////////// server ////////////////////////////////////////////
 
 function onMessage(msg,peer)
@@ -116,12 +194,25 @@ function onMessage(msg,peer)
       }
     
     if(data.command==='iamalive')
-      {// command:'iamalive', network:<name>, rol:'daemon'
+      {// command:'iamalive', network:<name>, who:<hostname>, rol:'daemon'
+       //
        if(Tools.isset(network))
          {
-          if(Tools.isset(data.rol) && data.rol==='daemon')
+          if(data.rol==='daemon')
             {
-             Level5.send(this,peer.address,peer.port, { command:"ping", network:data.network, rol:"broker", who:Os.hostname() });
+             if(!existsKey(objects,{type:'host',name:data.who}))
+               {
+                Level5.send(this,peer.address,peer.port, { command:"ping", network:data.network, rol:"broker", who:Os.hostname() });
+               }
+             else
+               {
+                var nid=insertOrUpdate(objects,{type:'network',name:data.network},{});
+                var hid=insertOrUpdate(objects,{type:'host',   name:data.who},    {});
+
+                insertOrUpdate(relations,{left:nid,right:hid,type:'contains'},{});
+                insertOrUpdate(properties,{object:hid,property:'address'},{value:peer.address});
+                insertOrUpdate(properties,{object:hid,property:'port'},{value:peer.port});
+               }
             }
          }
       }
@@ -129,18 +220,14 @@ function onMessage(msg,peer)
     if(data.command==='ping')
       {// command:'ping', network:<name>, rol:'daemon', who:<name>
        //
-       // recibimos un 'ping' desde 'who' en el papel de 'rol'
-       //
        if(Tools.isset(network) && rol==='daemon')
          {
-          Level5.send(this,peer.address,peer.port, { command:"pong", network:data.network, rol:'broker', who:Os.hostname(), from:data.who });
+          Level5.send(this,peer.address,peer.port, { command:"pong", network:data.network, rol:"broker", who:Os.hostname(), from:data.who });
          }
       }
     else  
     if(data.command==='pong')
       {// command:'pong', network:<name>, rol:'daemon', who:<name>, from:<name===Os.hostname()>
-       //
-       // recibimos un 'pong' desde 'who' en el papel de 'rol' y reclamado por 'from'
        //
        if(Tools.isset(network) && data.rol==='daemon' && data.from===Os.hostname())
          {
@@ -151,34 +238,34 @@ function onMessage(msg,peer)
           insertOrUpdate(properties,{object:hid,property:'address'},{value:peer.address});
           insertOrUpdate(properties,{object:hid,property:'port'},{value:peer.port});
 
-          Level5.send(this,peer.address,peer.port, { command:"status",  network:data.network, repeat:60 });
-          Level5.send(this,peer.address,peer.port, { command:"plugins", network:data.network            });
-         }
-      }
-    else  
-    if(data.command==='status')
-      {// command:'status', network:<name>, who:<name>, status:{ ... }
-       //
-       if(Tools.isset(network))
-         {
-          var nid=insertOrUpdate(objects,{type:'network',name:data.network},{});
-          var hid=insertOrUpdate(objects,{type:'host',   name:data.who},    {});
-          
-          if(Tools.isset(data.status))
-            {
-             for(var key in data.status)
-                {
-                 insertOrUpdate(properties,{object:hid,property:key},{value:data.status[key]});
-                }
-            } 
+          Level5.send(this,peer.address,peer.port, { command:"plugins", network:data.network  });
          }
       }
     else  
     if(data.command==='plugins')
-      {// command:'plugins', network:<name>, plugins:[ { name:<string>, description:<text> ... } ... ]
-       //
+      {// => command:'plugins', network:<name>, who:<hostname>, plugins:[ { name:<string>, description:<text>, delivers:[...] } ... ]
        if(Tools.isset(network))
          {
+          var nid=insertOrUpdate(objects,{type:'network',name:data.network},{});
+          var hid=insertOrUpdate(objects,{type:'host',   name:data.who},    {});
+
+          insertOrUpdate(relations,{left:nid,right:hid,type:'contains'},{});
+          insertOrUpdate(properties,{object:hid,property:'address'},{value:peer.address});
+          insertOrUpdate(properties,{object:hid,property:'port'},{value:peer.port});
+
+          for(var i in data.plugins)
+             {
+              var plugin=data.plugins[i];
+              var pid=insertOrUpdate(objects,{type:'plugin',name:plugin.name+'@'+data.who},{});
+              insertOrUpdate(relations,{left:hid,right:pid,type:'contains'},{description:plugin.description});
+              for(var key in plugin.delivers)
+                 {
+                  var deliver=plugin.delivers[key];
+                  insertOrUpdate(properties,{object:pid,property:key},{deliver:deliver,value:null});
+                 }
+             }
+
+          //console.log(Util.inspect(data));
          }
       }
    }
@@ -193,7 +280,7 @@ function onListening()
          for(var i in conf.networks)
             {
              var network=conf.networks[i];
-             Level5.send(server, network.broadcast, network.port, { command:"iamalive", network:network.name, rol:"broker" });
+             Level5.send(server, network.broadcast, network.port, { command:"iamalive", network:network.name, who:Os.hostname(), rol:"broker" });
             }
         }
      }
@@ -234,14 +321,23 @@ if(Tools.isset(conf.port))
   }
 
 /////////////////////////////////////////////////////////// web server ////////////////////////////////////////
+function webRequest(request,response)
+{
+ //TODO: mostrar lo que haya ante cualquier petición
+}
 
-//TODO: servidor web
+if(Tools.isset(conf.http))
+  {
+   var http=require('http').createServer();
+
+   http.on('',function(){});
+
+   http.on('request',webRequest);
+   http.on('close',function(){});
+   
+   http.listen(conf.http.bind.port,conf.http.bind.address);
+   console.log("HTTP listen on port "+conf.http.bind.port);
+  }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-process.on("SIGINT",
-           function()
-             {
-              console.log("Ending ...");
-              process.exit(0);
-             });
--- daemon/main.js
+++ daemon/main.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 /*************************************************************
+  MOONITOR daemon
   (c) 2012, Francisco Puentes (fran@puentescalvo.com)
 *************************************************************/
 var Util =require('util');
@@ -11,22 +12,69 @@ var Dgram=require('dgram');
 var NODE=process.argv[0];
 var SELF=Path.resolve(process.argv[1]);
 var CWD =Path.dirname(SELF);
+var PID =Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.pid');
+var KILL=Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.kill');
+var CONF=Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.conf');
 
 var Tools =require(Path.join(CWD,'common/tools.js' ));
 var Level5=require(Path.join(CWD,'common/level5.js'));
 
-//TODO: comprobar si existe un *.pid ejecutándose
-//      si es así, abandonar esta instancia
+var log=console.log;
 
-//create a '*.pid' file into CWD, with our PID
-Fs.writeFileSync(Path.join(CWD,Path.basename(SELF,Path.extname(SELF))+'.pid'),process.pid,'utf8');
-//'ps' will report this name
-process.title="moon-daemon";
+if(Fs.existsSync(PID))
+  {
+   try
+     {
+      var oldPid=parseInt(Fs.readFileSync(PID),10);
+      try
+        {
+         process.kill(oldPid,'SIGUSR2');
+         console.log("There are other instance of me working hard :-)");
+         process.exit(0);
+        }
+      catch(err)
+        {
+         Fs.unlinkSync(PID);
+        }
+     }
+   catch(err)
+     {
+      console.err(err);
+      console.err("Failed to load and parse '"+PID+"'");
+      process.exit(1);
+     }
+  }
+
+Fs.writeFileSync(PID,process.pid,'utf8');
+
+process.on('exit',
+           function()
+           {
+            console.log("Ending ...");
+            Fs.unlinkSync(PID);
+           });
+
+process.on('SIGINT',
+           function()
+           {
+            console.log("INT signal!");
+            process.exit(0);
+           });
+
+process.on('SIGUSR2',
+           function()
+           {
+            console.log("USR2 signal!");
+           });
+
+process.title="moonitor::daemon";
+
+//TODO: crear el script KILL que mata elegantemente esta instancia
 
 var conf=
     {
      //send 'iamalive' each 'announce' seconds (0 - disable)
-     announce: 10,
+     announce: 60,
      //bind to this port
      port:     1233,
      //network
@@ -35,11 +83,11 @@ var conf=
      plugins:  './plugins',
     };
 
-if(Fs.existsSync(Path.join(CWD,'daemon.conf.json')))
+if(Fs.existsSync(CONF))
   {
    try
      {
-      var tmp=Fs.readFileSync(Path.join(CWD,'daemon.conf.json'));
+      var tmp=Fs.readFileSync(CONF);
       conf=JSON.parse(tmp);
      }
    catch(err)  
@@ -55,39 +103,43 @@ for(var i=2; i<process.argv.length; i++)
     //TODO: procesar argumentos
    }
 
-/////////////////////////////////////////////////////////// plugins ///////////////////////////////////////////
-/*
-var PLUGINS=Path.resolve(CWD,conf.plugins);
-var plugins={};
-
-if(Fs.existsSync(PLUGINS))
-  {
-   var list=Fs.readdirSync(PLUGINS);
-   for(var i in list)
-      {
-       var entry=list[i];
-       var stat=Fs.statSync(entry);
-       if(stat.isFile())
-         {
-         }
-       else
-       if(stat.isDirectory())
-         {
-         }
-      }
-  }
-*/
 /////////////////////////////////////////////////////////// server ////////////////////////////////////////////
 //
 // commands:
-//   iamalive
-//   ping
-//   list
+//   iamalive *
+//   ping/pong *
+//   plugins
 //   get
 //   do
 //
 
 var broker={ address:undefined, port:undefined, lastUpdate:undefined };
+
+var timeouts ={};
+var intervals={};
+
+function removeInterval(key)
+{
+ if(Tools.isset(intervals[key]))
+   {
+    clearInterval(intervals[key]);
+    delete intervals[key];
+   }
+}
+
+function installInterval(key,repeat,times,callback/*...*/)
+{
+ removeInterval(key);
+ intervals[key]=setInterval(function(args)
+                            {
+                             times-=1;
+                             if(times>0) callback.apply(this,args);
+                             else        removeInterval(key);
+                            },
+                            repeat*1000,
+                            arguments.slice(4));
+
+}
 
 function onMessage(msg,peer)
 {
@@ -106,9 +158,11 @@ function onMessage(msg,peer)
       }
 
     if(data.command==='iamalive')
-      {// command:'iamalive', network:<name>, rol:'broker'
+      {// command:'iamalive', network:<name>, who:<hostname>, rol:'broker'
+       //
        if(Tools.isset(network) && data.rol==='broker')
          {
+          broker.hostname=data.who;
           broker.address=peer.address;
           broker.port=peer.port;
           broker.lastUpdate=new Date();
@@ -118,10 +172,12 @@ function onMessage(msg,peer)
     if(data.command==='ping')
       {// command:'ping', network:<name>, rol:'broker', who:<name>
        //
-       // recibimos un 'ping' desde 'who' en el papel de 'rol'
-       //
        if(Tools.isset(network) && data.rol==='broker')
          {
+          broker.hostname=data.who;
+          broker.address=peer.address;
+          broker.port=peer.port;
+          broker.lastUpdate=new Date();
           Level5.send(this,peer.address,peer.port, { command:"pong", network:data.network, rol:'daemon', who:Os.hostname(), from:data.who });
          }
       }
@@ -129,15 +185,86 @@ function onMessage(msg,peer)
     if(data.command==='pong')
       {// command:'pong', network:<name>, rol:'broker', who:<name>, from:<name===Os.hostname()>
        //
-       // recibimos un 'pong' desde 'who' en el papel de 'rol' y reclamado por 'from'
-       //
        if(Tools.isset(network) && data.rol==='broker' && data.from===Os.hostname())
          {
+          broker.hostname=data.who;
           broker.address=peer.address;
           broker.port=peer.port;
           broker.lastUpdate=new Date();
          }
       }
+    else
+    if(data.command==='plugins')
+      {// => command:'plugins', network:<name> [,repeat:<seconds>] [,times:<integer>]
+       // <= command:'plugins', network:<name>, who:<hostname>, plugins:[ { name:<string>, description:<text>, delivers:[...] } ... ]
+       //
+       if(Tools.isset(network) && peer.address===broker.address && peer.port===broker.port)
+         {
+          function plugins(server)
+            {
+             var plugins=[];
+             var PLUGINS=Path.resolve(conf.plugins);
+             var entries=Fs.readdirSync(PLUGINS);
+             for(var i in entries)
+                {
+                 var short=entries[i];
+                 var full =Path.join(PLUGINS,short);
+                 var stat =Fs.statSync(full);
+                 var name =null;
+                 var main =null;
+                 
+                 if(stat.isDirectory())
+                   {
+                    if(Fs.existsSync(Path.join(full,'main.js')))
+                      {
+                       main=Path.join(full,'main.js');
+                       name=short;
+                      }
+                   }
+                 else
+                 if(stat.isFile())
+                   {
+                    var re=new RegExp("^(.+)\.js$",'gi');
+                    var r;
+                    if((r=re.exec(short))!=null)
+                      {
+                       main=full;
+                       name=r[1];
+                      }
+                   }
+
+                 if(Tools.isset(main) && Tools.isset(name))
+                   {
+                    var plugin=require(main);
+                    if(Tools.isset(plugin.description) && (typeof plugin.description)==='string')
+                      {
+                       plugins.push({ name:name, description:plugin.description, delivers:plugin.delivers });
+                      }
+                   }
+                }
+                
+             Level5.send(server,broker.address,broker.port, { command:"plugins", network:data.network, who:Os.hostname(), plugins:plugins });
+            }
+             
+          plugins(this);
+          if(Tools.isset(data.repeat) && data.repeat>0)
+            {
+             var repeat=data.repeat;
+             var times=(Tools.isset(data.times)?data.times:0);
+             installInterval('plugins',repeat,times,plugins,this);
+            }
+         }
+      }
+    else
+    if(data.command==='get')
+      {// => command:'get', network:<name>, plugin:<name>, what:[name] [,repeat:<seconds>] [,times:<integer>]
+       // <= command:'get', network:<name>, plugin:<name>, what:<value>, who:<hostname>
+       //
+       if(Tools.isset(network) && peer.address===broker.address && peer.port===broker.port)
+         {
+         }
+      }
+    ///////////////////////////////////////////////////////////////////////////////////////////////      
    }
 }
 
@@ -147,7 +274,7 @@ function onListening()
      {
       if(Tools.isset(conf.network))
         {
-         Level5.send(server, conf.network.broadcast, conf.network.port, { command:"iamalive", network:conf.network.name, rol:"daemon" });
+         Level5.send(server, conf.network.broadcast, conf.network.port, { command:"iamalive", network:conf.network.name, who:Os.hostname(), rol:"daemon" });
         }
      }
  
